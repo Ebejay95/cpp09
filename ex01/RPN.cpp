c28d1eb7de40dd6ee2f2cf83caca13c3b51c4a2d
Jonathan Eberle <jeberle@student.42heilbronn.de>
2025-03-27 16:11:41 +0100

    at least working for now

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -2,31 +2,61 @@
 
 RPN::RPN() : error(0), error_message(""), walker(0) {}
 
-RPN::RPN(const RPN& src) {
-	*this = src;
+RPN::RPN(const RPN& src) : error(src.error), error_message(src.error_message), walker(src.walker) {
+	std::stack<double> temp = src.rpn_stack;
+	std::stack<double> reversed;
+
+	while (!temp.empty()) {
+		reversed.push(temp.top());
+		temp.pop();
+	}
+
+	while (!reversed.empty()) {
+		rpn_stack.push(reversed.top());
+		reversed.pop();
+	}
 }
 
 RPN& RPN::operator=(const RPN& src) {
 	if (this != &src) {
-		//
+		error = src.error;
+		error_message = src.error_message;
+		walker = src.walker;
+
+		while (!rpn_stack.empty())
+			rpn_stack.pop();
+
+		std::stack<double> temp = src.rpn_stack;
+		std::stack<double> reversed;
+
+		while (!temp.empty()) {
+			reversed.push(temp.top());
+			temp.pop();
+		}
+
+		while (!reversed.empty()) {
+			rpn_stack.push(reversed.top());
+			reversed.pop();
+		}
 	}
 	return *this;
 }
 
 RPN::~RPN() {}
 
-int RPN::is_numeric(char c)
-{
+int RPN::is_numeric(char c) {
 	return std::isdigit(c, std::locale());
 }
 
-int RPN::is_rpnchar(char c)
-{
+int RPN::is_decimal(char c) {
+	return (c == '.');
+}
+
+int RPN::is_rpnchar(char c) {
 	return (is_numeric(c) || c == ' ' || c == '/' || c == '+' || c == '*' || c == '-');
 }
 
-void RPN::rpn_filter(char val)
-{
+void RPN::rpn_filter(char val) {
 	if (!is_rpnchar(val))
 	{
 		error = 1;
@@ -40,20 +70,27 @@ void RPN::rpn_filter(char val)
 	return;
 }
 
-int RPN::success(int result)
-{
+int RPN::success(double result) {
 	std::cout << "\x1b[32mResult\033[0m: " << result << std::endl;
+	reset();
 	return 1;
 }
 
-int RPN::throw_error(std::string message)
-{
+int RPN::throw_error(std::string message) {
 	std::cout << "\x1b[31mError\033[0m: " << message << std::endl;
 	return 1;
 }
 
-int RPN::run(char *seed)
-{
+void RPN::reset(void) {
+	while (!rpn_stack.empty()) {
+		rpn_stack.pop();
+	}
+	error = 0;
+	error_message = "";
+	walker = 0;
+}
+
+int RPN::run(char *seed) {
 	while (seed[walker] != '\0' && !error)
 	{
 		rpn_filter(seed[walker]);
@@ -64,31 +101,62 @@ int RPN::run(char *seed)
 	}
 	if (walker <= 0)
 		return throw_error("empty seeds cannot be processed");
-	return success(42);
+	if (rpn_stack.size() != 1)
+		return throw_error("seed not resolvable");
+	return success(rpn_stack.top());
 }
 
-void RPN::rpn(char val)
-{
-	std::cout << "\x1b[33mrpn\033[0m: " << val << std::endl;
+void RPN::rpn(char val) {
 	if (is_numeric(val))
-	{
 		rpn_push_numeric(val);
-	}
 	else
-	{
 		rpn_push_operator(val);
-	}
 	return;
 }
 
-void RPN::rpn_push_numeric(char val)
-{
+void RPN::rpn_push_numeric(char val) {
+	rpn_stack.push(static_cast<double>(val - '0'));
 	std::cout << "\x1b[33mrpn_push_numeric\033[0m: " << val << std::endl;
 	return;
 }
 
-void RPN::rpn_push_operator(char val)
-{
-	std::cout << "\x1b[33mrpn_push_operator\033[0m: " << val << std::endl;
+double RPN::calculate(double first, char op, double second) {
+	double result = 0.0;
+
+	if (op == '+')
+		result = first + second;
+	else if (op == '-')
+		result = first - second;
+	else if (op == '*')
+		result = first * second;
+	else if (op == '/') {
+		if (second == 0) {
+			error = 1;
+			error_message = "division by zero";
+			return 0;
+		}
+		result = first / second;
+	}
+
+	return result;
+}
+
+void RPN::rpn_push_operator(char op) {
+	if (rpn_stack.size() < 2)
+	{
+		error = 1;
+		error_message = "seed not resolvable (operator requires operants)";
+		return;
+	}
+
+	double second = rpn_stack.top();
+	rpn_stack.pop();
+	double first = rpn_stack.top();
+	rpn_stack.pop();
+
+	std::cout << "\x1b[33mrpn_push_operator\033[0m: " << op << std::endl;
+	std::cout << "\x1b[34mrpn_perform\033[0m: " << first << " "<< op << " " << second << std::endl;
+
+	rpn_stack.push(calculate(first, op, second));
 	return;
 }
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -5,27 +5,32 @@
 #include <stack>
 #include <string>
 #include <locale>
+#include <limits>
 
 class RPN {
 	private:
-		int				error;
-		std::string		error_message;
-		int				walker;
-		int				is_numeric(char c);
-		int				is_rpnchar(char c);
-		void			rpn(char val);
-		void			rpn_filter(char val);
-		void			rpn_push_numeric(char val);
-		void			rpn_push_operator(char val);
+		std::stack<double>		rpn_stack;
+		int						error;
+		std::string				error_message;
+		int						walker;
+		int						is_numeric(char c);
+		int						is_decimal(char c);
+		int						is_rpnchar(char c);
+		void					rpn(char val);
+		void					rpn_filter(char val);
+		void					rpn_push_numeric(char val);
+		void					rpn_push_operator(char val);
+		double					calculate(double first, char op, double second);
+		void					reset(void);
 
 	public:
-						RPN();
-						RPN(const RPN& src);
-						RPN& operator=(const RPN& src);
-						~RPN();
-		int				success(int result);
-		int				throw_error(std::string message);
-		int				run(char *seed);
+								RPN();
+								RPN(const RPN& src);
+								RPN& operator=(const RPN& src);
+								~RPN();
+		int						success(double result);
+		int						throw_error(std::string message);
+		int						run(char *seed);
 };
 
 #endif
