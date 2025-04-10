8af93539c0dc47d6f5fbd2a5f298272df93d09ea
Jonathan Eberle <jeberle@student.42heilbronn.de>
2025-04-10 13:47:21 +0200

    template functions yay!

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -11,7 +11,7 @@ PmergeMe::PmergeMe() : error(0), error_message(""), walker(0) {}
 
 PmergeMe::PmergeMe(const PmergeMe& src) :
 	pmerge_me_vector(src.pmerge_me_vector),
-	pmerge_me_queue(src.pmerge_me_queue),
+	pmerge_me_deque(src.pmerge_me_deque),
 	error(src.error),
 	error_message(src.error_message),
 	walker(src.walker) {
@@ -20,7 +20,7 @@ PmergeMe::PmergeMe(const PmergeMe& src) :
 PmergeMe& PmergeMe::operator=(const PmergeMe& src) {
 	if (this != &src) {
 		pmerge_me_vector = src.pmerge_me_vector;
-		pmerge_me_queue = src.pmerge_me_queue;
+		pmerge_me_deque = src.pmerge_me_deque;
 		error = src.error;
 		error_message = src.error_message;
 		walker = src.walker;
@@ -51,121 +51,12 @@ int PmergeMe::throw_error(std::string message) {
 	return 1;
 }
 
-void PmergeMe::fill_queue(char *arg) {
-	std::stringstream ss(arg);
-	int num;
-	ss >> num;
-	pmerge_me_queue.push(num);
-}
-
-void PmergeMe::fill_vector(char *arg) {
+void PmergeMe::fill_containers(char *arg) {
 	std::stringstream ss(arg);
 	int num;
 	ss >> num;
 	pmerge_me_vector.push_back(num);
-}
-
-void PmergeMe::swap_elements(std::vector<int>& container, size_t el_size, size_t prev_size, size_t i) {
-	size_t last_first_half = i + prev_size - 1;
-	size_t last_second_half = std::min(i + el_size - 1, container.size() - 1);
-	if (container[last_first_half] > container[last_second_half]) {
-		size_t second_half_size = std::min(prev_size, container.size() - (i + prev_size));
-		for (size_t j = 0; j < second_half_size; j++) {
-			std::swap(container[i + j], container[i + prev_size + j]);
-		}
-	}
-}
-
-void PmergeMe::sort_in_elements(std::vector<int>& container, size_t el_size) {
-	if (el_size == 2) {
-		for (size_t i = 0; i < container.size(); i += 2) {
-			if (i + 1 < container.size() && container[i] > container[i+1]) {
-				std::swap(container[i], container[i+1]);
-			}
-		}
-	}
-	else {
-		size_t prev_size = el_size / 2;
-		for (size_t i = 0; i < container.size(); i += el_size) {
-			if (i + prev_size < container.size()) {
-				swap_elements(container, el_size, prev_size, i);
-			}
-		}
-	}
-	for (size_t i = 0; i < container.size(); i += el_size) {
-		for (size_t j = i; j < i + el_size && j < container.size(); j++) {
-			std::cout << Y << container[j] << " " << D;
-		}
-		std::cout << std::endl;
-	}
-}
-
-
-void PmergeMe::leftover_extract(std::vector<int>& leftover, std::vector<int>& container, bool has_leftover, int count_leftover) {
-	std::cout << G << "leftover_extract" << D << std::endl;
-	if (has_leftover) {
-		while (count_leftover) {
-			leftover.push_back(container.back());
-			container.pop_back();
-			count_leftover--;
-		}
-	}
-}
-
-void PmergeMe::leftover_repair(std::vector<int>& leftover, std::vector<int>& container, bool has_leftover, int count_leftover) {
-
-	std::cout << G << "leftover_repair" << D << std::endl;
-	if (has_leftover) {
-		while (count_leftover) {
-			container.push_back(leftover.back());
-			leftover.pop_back();
-			count_leftover--;
-		}
-	}
-}
-
-
-void PmergeMe::create_elements(std::vector<int>& container, size_t& el_size) {
-	if (container.size() <= 1) {
-		return;
-	}
-	std::cout << G << "create_elements" << D << std::endl;
-
-	bool has_leftover = (container.size() % el_size != 0);
-	int count_leftover = container.size() % el_size;
-	std::vector<int> leftover;
-
-	leftover_extract(leftover, container, has_leftover, count_leftover);
-	print_container(container);
-	std::cout << ": ";
-	print_container_ln(leftover);
-
-	sort_in_elements(container, el_size);
-
-	leftover_repair(leftover, container, has_leftover, count_leftover);
-	print_container_ln(container);
-	el_size *= 2;
-	// std::cout << "el_size: " << el_size << std::endl;
-	// std::cout << "count_leftover: " << count_leftover << std::endl;
-	// std::cout << "container.size(): " << container.size() << std::endl;
-	if ((el_size * 2) < container.size())
-	{
-		create_elements(container, el_size);
-	}
-}
-
-double PmergeMe::ford_johnson_sort(std::vector<int>& container) {
-	std::chrono::steady_clock::time_point start;
-	size_t el_size = 2;
-
-	start = std::chrono::high_resolution_clock::now();
-
-	if (container.size() <= 1)
-		return processeing_time(start);
-
-	create_elements(container, el_size);
-
-	return processeing_time(start);
+	pmerge_me_deque.push_back(num);
 }
 
 int PmergeMe::run(int argc, char *argv[]) {
@@ -176,29 +67,30 @@ int PmergeMe::run(int argc, char *argv[]) {
 	{
 		if (!is_numeric(argv[walker]))
 			return throw_error(std::string("each argument must be numeric unsigned and \"") + argv[walker] + "\" is not");
-		//fill_queue(argv[walker]);
-		fill_vector(argv[walker]);
+		fill_containers(argv[walker]);
 		walker++;
 	}
-	//double time_queue = 0;
+	double time_deque = 0;
 	double time_vector = 0;
-	//print_before_sort(pmerge_me_queue, "std::queue<int>");
+	print_before_sort(pmerge_me_deque, "std::deque<int>");
 	print_before_sort(pmerge_me_vector, "std::vector<int>");
-//	time_queue = ford_johnson_sort(pmerge_me_queue);
+	time_deque = ford_johnson_sort(pmerge_me_deque);
 	time_vector = ford_johnson_sort(pmerge_me_vector);
-	//print_after_sort(pmerge_me_queue, "std::queue<int>", time_queue);
+	print_after_sort(pmerge_me_deque, "std::deque<int>", time_deque);
 	print_after_sort(pmerge_me_vector, "std::vector<int>", time_vector);
 	return 0;
 }
 
 /*
-dynamic cast
-
-
+create elements
 12 9 0 5 17 1 3 16 7 4 43 8 64 2 34 6 45 90 11
 (12 9) (0 5) (17 1) (3 16) (7 4) (43 8) (64 2) (34 6) (45 90) 11
 (9 12) (0 5) (1 17) (3 16) (4 7) (8 43) (2 64) (6 34) (45 90) 11
 (9 12) (0 5) (1 17) (3 16) (4 7) (8 43) (2 64) (6 34) (45 90) 11
 ((9 12) (0 5)) ((1 17) (3 16)) ((4 7) (8 43)) ((2 64) (6 34)) (45 90) 11
+((0 5) (9 12)) ((3 16) (1 17)) ((4 7) (8 43)) ((6 34) (2 64)) (45 90) 11
+(((0 5) (9 12)) ((3 16) (1 17))) (((4 7) (8 43)) ((6 34) (2 64))) (45 90) 11
+
+merge elements
 ((0 5) (9 12)) ((3 16) (1 17)) ((4 7) (8 43)) ((6 34) (2 64)) (45 90) 11
 */
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -3,13 +3,12 @@
 
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <deque>
 #include <string>
 #include <locale>
 #include <sstream>
 #include <chrono>
 
-// Define Colours
 # define G "\033[0;32m"
 # define B "\033[0;34m"
 # define Y "\033[0;93m"
@@ -25,32 +24,6 @@ void print_container(const T& container) {
 	for (it = container.begin(); it != container.end(); ++it) {
 		std::cout << *it << " ";
 	}
-}
-
-inline void print_container(const std::queue<int>& q) {
-	std::queue<int> temp = q;
-	while (!temp.empty()) {
-		std::cout << temp.front() << " ";
-		temp.pop();
-	}
-}
-
-template <typename T>
-void print_container_ln(const T& container) {
-	typename T::const_iterator it;
-	for (it = container.begin(); it != container.end(); ++it) {
-		std::cout << *it << " ";
-	}
-	std::cout << std::endl;
-}
-
-inline void print_container_ln(const std::queue<int>& q) {
-	std::queue<int> temp = q;
-	while (!temp.empty()) {
-		std::cout << temp.front() << " ";
-		temp.pop();
-	}
-	std::cout << std::endl;
 }
 
 template <typename T>
@@ -72,26 +45,121 @@ void print_after_sort(const T& container, const std::string& container_name, dou
 
 double processeing_time(std::chrono::steady_clock::time_point start);
 
+
+template <typename T>
+void swap_elements(T& container, size_t el_size, size_t prev_size, size_t i) {
+	size_t last_first_half = i + prev_size - 1;
+	size_t last_second_half = std::min(i + el_size - 1, container.size() - 1);
+	if (container[last_first_half] > container[last_second_half]) {
+		size_t second_half_size = std::min(prev_size, container.size() - (i + prev_size));
+		for (size_t j = 0; j < second_half_size; j++) {
+			std::swap(container[i + j], container[i + prev_size + j]);
+		}
+	}
+}
+
+template <typename T>
+void sort_in_elements(T& container, size_t el_size) {
+	if (el_size == 2) {
+		for (size_t i = 0; i < container.size(); i += 2) {
+			if (i + 1 < container.size() && container[i] > container[i+1]) {
+				std::swap(container[i], container[i+1]);
+			}
+		}
+	}
+	else {
+		size_t prev_size = el_size / 2;
+		for (size_t i = 0; i < container.size(); i += el_size) {
+			if (i + prev_size < container.size()) {
+				swap_elements(container, el_size, prev_size, i);
+			}
+		}
+	}
+}
+
+template <typename T>
+void leftover_extract(T& leftover, T& container, bool has_leftover, int count_leftover) {
+	if (has_leftover) {
+		while (count_leftover) {
+			leftover.push_back(container.back());
+			container.pop_back();
+			count_leftover--;
+		}
+	}
+}
+
+template <typename T>
+void leftover_repair(T& leftover, T& container, bool has_leftover, int count_leftover) {
+
+	if (has_leftover) {
+		while (count_leftover) {
+			container.push_back(leftover.back());
+			leftover.pop_back();
+			count_leftover--;
+		}
+	}
+}
+
+template <typename T>
+void create_elements(T& container, size_t& el_size) {
+	if (container.size() <= 1) {
+		return;
+	}
+
+	bool has_leftover = (container.size() % el_size != 0);
+	int count_leftover = container.size() % el_size;
+	T leftover;
+
+	leftover_extract(leftover, container, has_leftover, count_leftover);
+	sort_in_elements(container, el_size);
+	leftover_repair(leftover, container, has_leftover, count_leftover);
+
+	el_size *= 2;
+
+	if ((el_size * 2) < container.size())
+		create_elements(container, el_size);
+}
+
+template <typename T>
+void merge_elements(T& container, size_t& el_size) {
+	std::cout << G << "merge_elements " << el_size << D << std::endl;
+	print_container(container);
+	std::cout << "\n";
+
+	// T a;
+	// T b;
+	// el_size /= 2;
+}
+
+template <typename T>
+double ford_johnson_sort(T& container) {
+	std::chrono::steady_clock::time_point start;
+	size_t el_size = 2;
+
+	start = std::chrono::high_resolution_clock::now();
+
+	if (container.size() <= 1)
+		return processeing_time(start);
+
+	create_elements(container, el_size);
+
+	merge_elements(container, el_size);
+
+	return processeing_time(start);
+}
+
+
 class PmergeMe {
 	private:
 		std::vector<int>	pmerge_me_vector;
-		std::queue<int> 	pmerge_me_queue;
+		std::deque<int> 	pmerge_me_deque;
 		int					error;
 		std::string			error_message;
 		int					walker;
 		int					is_numeric(char *arg);
 		int					is_decimal(char c);
 		int					throw_error(std::string message);
-		void 				fill_queue(char *arg);
-		void 				fill_vector(char *arg);
-
-		void leftover_extract(std::vector<int>& leftover, std::vector<int>& container, bool has_leftover, int count_leftover);
-		void leftover_repair(std::vector<int>& leftover, std::vector<int>& container, bool has_leftover, int count_leftover);
-		void swap_elements(std::vector<int>& container, size_t el_size, size_t prev_size, size_t i);
-		void sort_in_elements(std::vector<int>& container, size_t pair_size);
-		void create_elements(std::vector<int>& container, size_t& el_size);
-		double ford_johnson_sort(std::vector<int>& container);
-		double ford_johnson_sort(std::queue<int>& container);
+		void				fill_containers(char *arg);
 
 	public:
 							PmergeMe();
